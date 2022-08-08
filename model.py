c4a0843fdc4e4088f9d1e9543713dd9ae4fedaad
JJY <jjy1030@126.com>
2022-08-08 11:25:44 +0800

    残差块并联、进入RFB、5层

--- model.py
+++ model.py
@@ -1,5 +1,4 @@
 import math
-from pyexpat import model
 import torch
 from torch import nn
 from CBAM import CBAM
@@ -14,23 +13,20 @@ class Generator(nn.Module):
             nn.Conv2d(3, 64, kernel_size=9, padding=4),
             nn.PReLU()
         )
-        
         self.block2 = ResidualBlock(64)
         self.block3 = ResidualBlock(64)
         self.block4 = ResidualBlock(64)
         self.block5 = ResidualBlock(64)
         self.block6 = ResidualBlock(64)
-        self.block7 = ResidualBlock(64)
-        self.block8 = ResidualBlock(64)
-        self.block9 = ResidualBlock(64)
-
-        self.block10 = nn.Sequential(
+        self.block7 = nn.Sequential(
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64)
         )
-        block11 = [UpsampleBLock(64, 2) for _ in range(upsample_block_num)]
-        block11.append(nn.Conv2d(64, 3, kernel_size=9, padding=4))
-        self.block11 = nn.Sequential(*block11)
+        self.rfb = ReceptiveFieldBlock(64,64)
+        block8 = [UpsampleBLock(64, 2) for _ in range(upsample_block_num)]
+        block8.append(nn.Conv2d(64, 3, kernel_size=9, padding=4))
+        self.block8 = nn.Sequential(*block8)
+
     def forward(self, x):
         block1 = self.block1(x)
         block2 = self.block2(block1)
@@ -39,15 +35,15 @@ class Generator(nn.Module):
         block5 = self.block5(block4)
         block6 = self.block6(block5)
         block7 = self.block7(block6)
-        block8 = self.block8(block7)
-        block9 = self.block9(block8)
-        block10 = self.block10(block9)
-        block11 = self.block11(block1 + block10)
 
-        feature = torch.add(block11, block11)
-        feature = (torch.tanh(feature) + 1) / 2
+        block_add = torch.add(block1 + block7,block1 + block7)
+        
+        block8 = self.rfb(block_add)
 
-        return feature
+        block9 = self.block8(block8)
+
+        return (torch.tanh(block9) + 1) / 2
+
 
 class Discriminator(nn.Module):
     def __init__(self):
@@ -55,47 +51,38 @@ class Discriminator(nn.Module):
         self.net = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=3, padding=1),
             nn.LeakyReLU(0.2),
-            # CBAM(64),
 
             nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(64),
             nn.LeakyReLU(0.2),
-            # CBAM(64),
 
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2),
-            # CBAM(128),
 
             nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2),
-            # CBAM(128),
 
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
             nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2),
-            # CBAM(256),
 
             nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2),
-            # CBAM(256),
 
             nn.Conv2d(256, 512, kernel_size=3, padding=1),
             nn.BatchNorm2d(512),
             nn.LeakyReLU(0.2),
-            # CBAM(512),
 
             nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(512),
             nn.LeakyReLU(0.2),
-            # CBAM(512),
 
             nn.AdaptiveAvgPool2d(1),
             nn.Conv2d(512, 1024, kernel_size=1),
             nn.LeakyReLU(0.2),
-            CBAM(1024),
             nn.Conv2d(1024, 1, kernel_size=1)
         )
 
@@ -104,32 +91,47 @@ class Discriminator(nn.Module):
         return torch.sigmoid(self.net(x).view(batch_size))
 
 
+class ResidualBlock(nn.Module):
+    def __init__(self, channels):
+        super(ResidualBlock, self).__init__()
+        self.RSBU_CW = RSBU_CW(channels, channels)
+
+    def forward(self, x):
+        residual = self.RSBU_CW(x)
+
+        return x + residual
+
+# class ResidualBlock(nn.Module):
+#     def __init__(self, channels):
+#         super(ResidualBlock, self).__init__()
+#         self.conv1 = nn.Conv2d(channels, channels, kernel_size=3, padding=1)
+#         self.bn1 = nn.BatchNorm2d(channels)
+#         self.prelu = nn.PReLU()
+#         self.conv2 = nn.Conv2d(channels, channels, kernel_size=3, padding=1)
+#         self.bn2 = nn.BatchNorm2d(channels)
+
+#     def forward(self, x):
+#         residual = self.conv1(x)
+#         residual = self.bn1(residual)
+#         residual = self.prelu(residual)
+#         residual = self.conv2(residual)
+#         residual = self.bn2(residual)
+
+#         return x + residual
+
+
 class UpsampleBLock(nn.Module):
     def __init__(self, in_channels, up_scale):
         super(UpsampleBLock, self).__init__()
         self.conv = nn.Conv2d(in_channels, in_channels * up_scale ** 2, kernel_size=3, padding=1)
         self.pixel_shuffle = nn.PixelShuffle(up_scale)
         self.prelu = nn.PReLU()
-        self.rfb = ReceptiveFieldBlock(in_channels, in_channels)
 
     def forward(self, x):
-        x = self.rfb(x)
         x = self.conv(x)
         x = self.pixel_shuffle(x)
         x = self.prelu(x)
         return x
-
-class ResidualBlock(nn.Module):
-    def __init__(self, channels):
-        super(ResidualBlock, self).__init__()
-        # self.CBAM = CBAM(channels)
-        self.RSBU_CW = RSBU_CW(channels, channels)
-
-    def forward(self, x):
-        residual = self.RSBU_CW(x)
-        # residual = self.CBAM(residual)
-
-        return x + residual
 
 class ReceptiveFieldBlock(nn.Module):
     def __init__(self, in_channels: int, out_channels: int):
